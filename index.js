9762b0a6700fde1b49193dfb1eac9393170cbd3d
Anthony At Zinid <anthony@zinid.com>
2017-02-19 11:35:00 -0600

    Refactor: Decoupling implicit outter scope use

--- index.js
+++ index.js
@@ -2,6 +2,77 @@
 const _ = require('lodash')
 const sharp = require('sharp')
 
+// Returns an object with params from file following pattern defined in README.md
+function imagenameParams(imagename, picPattern, reParam, opts) {
+	// Non-repeating params
+	const basicParams = imagename.match(picPattern)
+	let params = {
+		name: basicParams[1],
+		ext: basicParams[3]
+	}
+
+	// Repeating params
+	const paramPattern = new RegExp(`${reParam}`, 'g')
+	let auxParam
+	while ((auxParam = paramPattern.exec(imagename)) !== null) {
+		const paramName = auxParam[0].match(/[a-z]+/)[0]
+		let paramValue = auxParam[0].match(/[0-9,]+/)[0]
+		// Special param: Width is an array
+		if ('w' === paramName) {
+			paramValue = _.map(paramValue.split(','), (val) => Number(val))
+		} else {
+			paramValue = Number(paramValue)
+		}
+
+		params[paramName] = paramValue
+	}
+
+	// Add in default file params
+	const defaults = { jpg: opts.jpg, webp: opts.webp }
+	params = _.assignIn(defaults, params)
+
+	// Return constructed params object
+	return params
+}
+
+// Returns promise to insert new image into Metalsmith `files` object with `params`:
+// * name: Image filename prefix
+// * width: How wide the image should be
+// * ext: What file extension the image should be
+// * quality: What quality the image should be (For .jpg & .webp)
+function createImage(files, params, opts) {
+	const newname = `${params.name}-${params.width}.${params.ext}`
+	const newpath = `${opts.path}/${newname}`
+
+	// Resize the image and put in appropriate format / quality
+	let s = sharp(params.buffer).resize(params.width)
+	switch (params.ext) {
+		case 'png':
+			s = s.png()
+			break
+		case 'jpg':
+			s = s.jpeg({ quality: params.quality })
+			break
+		case 'webp':
+			s = s.webp({
+				quality: params.quality,
+				lossless: (100 === params.quality)
+			})
+			break
+	}
+
+	// Return promise to actually create the buffer as new Metalsmith file
+	return new Promise((resolve, reject) => {
+		s.toBuffer((err, buffer, info) => {
+			if (err) {
+				reject(err)
+			}
+			files[newpath] = { contents: buffer }
+			resolve()
+		})
+	})
+}
+
 function plugin(options) {
 	// Options and defaults
 	const opts = options || {}
@@ -21,77 +92,6 @@ function plugin(options) {
 	const reExt = 'jpg|png'
 	const picPattern = new RegExp(`${opts.path}/(${reName})(${reParam})*\\.(${reExt})`)
 
-	// Returns an object with params from file following pattern defined in README.md
-	function imagenameParams(imagename) {
-		// Non-repeating params
-		const basicParams = imagename.match(picPattern)
-		let params = {
-			name: basicParams[1],
-			ext: basicParams[3]
-		}
-
-		// Repeating params
-		const paramPattern = new RegExp(`${reParam}`, 'g')
-		let auxParam
-		while ((auxParam = paramPattern.exec(imagename)) !== null) {
-			const paramName = auxParam[0].match(/[a-z]+/)[0]
-			let paramValue = auxParam[0].match(/[0-9,]+/)[0]
-			// Special param: Width is an array
-			if ('w' === paramName) {
-				paramValue = _.map(paramValue.split(','), (val) => Number(val))
-			} else {
-				paramValue = Number(paramValue)
-			}
-
-			params[paramName] = paramValue
-		}
-
-		// Add in default file params
-		const defaults = { jpg: opts.jpg, webp: opts.webp }
-		params = _.assignIn(defaults, params)
-
-		// Return constructed params object
-		return params
-	}
-
-	// Returns promise to insert new image into Metalsmith `files` object with `params`:
-	// * name: Image filename prefix
-	// * width: How wide the image should be
-	// * ext: What file extension the image should be
-	// * quality: What quality the image should be (For .jpg & .webp)
-	function createImage(files, params) {
-		const newname = `${params.name}-${params.width}.${params.ext}`
-		const newpath = `${opts.path}/${newname}`
-
-		// Resize the image and put in appropriate format / quality
-		let s = sharp(params.buffer).resize(params.width)
-		switch (params.ext) {
-			case 'png':
-				s = s.png()
-				break
-			case 'jpg':
-				s = s.jpeg({ quality: params.quality })
-				break
-			case 'webp':
-				s = s.webp({
-					quality: params.quality,
-					lossless: (100 === params.quality)
-				})
-				break
-		}
-
-		// Return promise to actually create the buffer as new Metalsmith file
-		return new Promise((resolve, reject) => {
-			s.toBuffer((err, buffer, info) => {
-				if (err) {
-					reject(err)
-				}
-				files[newpath] = { contents: buffer }
-				resolve()
-			})
-		})
-	}
-
 	// The actual plugin returned from `.use(...)` call
 	return function(files, metalsmith, done) {
 		const promisesToCreateImages = []
@@ -103,7 +103,7 @@ function plugin(options) {
 				removeFilenames.push(filename)
 
 				// Gather params from filename
-				const params = imagenameParams(filename)
+				const params = imagenameParams(filename, picPattern, reParam, opts)
 
 				// Set default params for new image
 				const defs = {
@@ -124,7 +124,8 @@ function plugin(options) {
 								defs,
 								// Passed in options
 								customOpts
-							)
+							),
+							opts
 						)
 					)
 				}
